dc8c1907029faa37db31e20d1908c723d7d7ab44
Jordan Senko <oq066776@gmail.com>
2025-07-01 16:49:06 -0600

    almost done experience section

--- src/components/experience/XP.jsx
+++ src/components/experience/XP.jsx
@@ -4,24 +4,23 @@ import styles from "./XP.module.css";
 const XP = () => {
     return (
         <>
-        <div className={styles.XPContainer}>
-            <div className={styles.headerContainer}>
-                <p>Explore My</p>
-                <h1>Experinece</h1>
+            <div className={styles.XPContainer}>
+                <div className={styles.headerContainer}>
+                    <p>Explore My</p>
+                    <h1>Experinece</h1>
+                </div>
+                <div className={styles.xpCardsContainer}>
+                    <XPCard
+                        title="Back-End Dev"
+                        skills={["MySQL", "NodeJS", "Git", "Python", "Java", "C++", "C", "Assembly"]}
+                    />
+                    <XPCard
+                        title="Front-End Dev"
+                        skills={["HTML", "CSS", "Javascript", "React", "Vercel"]}
+                    />
+                </div>
+
             </div>
-            <div className={styles.cardContainers}>
-            </div>
-                <XPCard 
-                    title="Back-End Dev"
-                    skills={["MySQL", "NodeJS", "Git", "Python", "Java", "C++", "C", "Assembly"]}
-                />
-                
-                {/* <XPCard 
-                    title="Front-End Dev"
-                    skills={["HTML", "CSS", "Javascript", "React", "Vercel"]}
-                > */}
-                
-        </div>
         </>
     );
 }
--- src/components/experience/xp-card/XPCard.jsx
+++ src/components/experience/xp-card/XPCard.jsx
@@ -4,16 +4,20 @@ const XPCard = (props) => {
     /* ".map(FOR EACH skill IN skills, DO <li key=..."*/
     /* index is the CURRENT INDEX of the loop from . 
     /* .map() SYNTAX: .map(currVal (req), index (opt), array (opt))*/
-    const skills = props.skills.map((skills, index) => <li key={index}>{skills}</li>);
+    const skills = props.skills.map((skills, index) => 
+        <div className={styles.skill}>
+            <i className="fa-solid fa-square-check"></i>
+            <li key={index}>{skills}</li>
+        </div>
+    );
 
-    return(
+    return (
         <>
             <div className={styles.xpCardContainer}>
                 <h2>{props.title}</h2>
                 <div className={styles.skillsContainer}>
-                    <ol>{skills}</ol>
+                    {skills}
                 </div>
-                
             </div>
         </>
     );
